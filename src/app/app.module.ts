07f1277a0fc4d0d51f7a526b1ca39eea200bab7d
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-12-15 14:14:22 +0900

    BrowserAnimationsModule をimport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { PortalModule } from '@angular/cdk/portal';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ToastComponent } from './toast/toast.component';
 
 @NgModule({
   declarations: [AppComponent, ToastComponent],
-  imports: [BrowserModule, PortalModule, OverlayModule],
+  imports: [
+    BrowserModule,
+    PortalModule,
+    OverlayModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [ToastComponent],
