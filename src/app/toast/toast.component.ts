0c8d3e1d5dc4ac5c58c04f526f8ebae61e860ed9
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-12-15 14:19:38 +0900

    普通にanimationを書いてみる

--- src/app/toast/toast.component.ts
+++ src/app/toast/toast.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
-  styleUrls: ['./toast.component.scss']
+  styleUrls: ['./toast.component.scss'],
+  animations: [
+    trigger('isVisible', [
+      state('visible', style({ transform: 'translateY(0)' })),
+      state('hidden', style({ transform: 'translateY(72px)' })),
+      transition(':enter', [
+        style({ transform: 'translateY(72px)' }),
+        animate(100),
+      ]),
+      transition(':leave', [
+        animate(100, style({ transform: 'translateY(72px)' })),
+      ]),
+    ]),
+  ],
 })
-export class ToastComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ToastComponent {
+  constructor() {}
 }
