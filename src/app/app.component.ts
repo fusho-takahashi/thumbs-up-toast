5c9b254665f4e406113b9ad4423b39872cc9e282
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-12-15 13:22:59 +0900

    高評価ボタンを実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastService } from './toast/toast.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'thumbs-up-toast';
+  constructor(private toastService: ToastService) {}
+
+  toastOpen() {
+    this.toastService.open();
+  }
 }
