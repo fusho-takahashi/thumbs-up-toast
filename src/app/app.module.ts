89a798ac8188146f955f5e343a9dd9352b815989
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-12-15 12:20:01 +0900

    portal とoverlay のmodule をimport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PortalModule } from '@angular/cdk/portal';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, PortalModule, OverlayModule],
   providers: [],
   bootstrap: [AppComponent],
 })
